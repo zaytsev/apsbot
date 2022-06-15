692bca91c9c351913097709492a48ef5b4e23206
Sergey Zaytsev <sergey@zaytseff.net>
2022-06-15 21:25:13 +0700

    feat: Intial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Basic Rust dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat, fenix }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ fenix.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (
+              with fenix.packages.${system};
+              combine [
+                stable.rustc
+                stable.cargo
+                stable.rust-src
+                stable.rustfmt
+                rust-analyzer
+              ]
+            )
+            cargo-edit
+            cargo-update
+            cargo-geiger
+            cargo-outdated
+            cargo-audit
+            
+            cocogitto
+          ];
+        };
+      }
+    );
+}
